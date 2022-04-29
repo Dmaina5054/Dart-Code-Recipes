3f9df18560b14ed8b2b723579a2b90076b64c003
Dmaina5054 <dmain5054@gmail.com>
2022-04-29 13:25:10 +0300

    added class

---
+++ classes/HeroClass.dart
@@ -0,0 +1,37 @@
+//defining a class
+class Hero {
+  //some attributes
+  String firstname;
+  String lastname;
+  //static field does not require class initialization
+  static String species = 'Human';
+
+  //constructor
+  Hero(this.firstname, this.lastname);
+
+  String get speci => species;
+
+  //a method
+  Future<String> getSpecies() async {
+    //simulate async
+    print('Almost there...');
+    await Future.delayed(Duration(seconds: 2));
+
+    //perform get
+    return speci;
+  }
+}
+
+//defining main
+
+main() async {
+  //initializing hero class
+  Hero myHero = Hero('James', 'Legend');
+
+  //testing async
+
+  String specie = await myHero.getSpecies();
+  print('Generating species...');
+  print(specie);
+  print('Call complete');
+}
